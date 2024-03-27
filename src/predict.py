3a9cea5cd3c78b4bd93cd9442d14ba1d94645858
Opu <nahidanam404@gmail.com>
2024-03-27 09:37:39 +0600

    Final Project submission

---
+++ src/predict.py
@@ -0,0 +1,24 @@
+import torch
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from PIL import Image
+
+from model import *
+
+def predict_image(model, image_path, device):
+    transform = transforms.Compose([
+        transforms.Resize(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    
+    
+    image = Image.open(image_path)
+    image_tensor = transform(image).unsqueeze(0).to(device)
+    model.eval()
+    with torch.no_grad():
+        output = model(image_tensor)
+        _, predicted = torch.max(output.data, 1)
+    class_names = ['bike', 'car']
+    prediction = class_names[predicted.item()]
+    return prediction
